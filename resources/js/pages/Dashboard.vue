6b13a6ff288b69b41dc96dac3d45a1e1b1e5067c
Mehedi Hasan Hasib <mehedihasanhasib.dev@gmail.com>
2026-02-25 22:59:19 +0600

    new page header component added

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import {
+    PageDescription,
+    PageHeader,
+    PageHeaderLeft,
+    PageHeaderRight,
+    PageTitle,
+} from "@/components/page-header";
+import { Button } from "@/components/ui/button";
 import AppLayout from "@/layouts/AppLayout.vue";
 </script>
 
 <template>
     <AppLayout>
-        <div>Dashboard</div>
+        <PageHeader>
+            <PageHeaderLeft>
+                <PageTitle>Users</PageTitle>
+                <PageDescription>Manage all users here</PageDescription>
+            </PageHeaderLeft>
+
+            <PageHeaderRight>
+                <Button>Add User</Button>
+            </PageHeaderRight>
+        </PageHeader>
     </AppLayout>
 </template>
